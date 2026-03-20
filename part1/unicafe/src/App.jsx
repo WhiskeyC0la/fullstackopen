be1ea493d32ec8ef8d86ef9f6426aa88ddb752f5
WhiskeyCola <jackcygan@gmail.com>
2026-03-20 12:26:19 +0200

    part1 exercise 1.7 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-const Header = ({text}) => <h1>{text}</h1>
-const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
+
+const Header = ({ text }) => <h1>{text}</h1>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : Number(((good - bad) / total).toFixed(2))
+  const positive = total === 0 ? 0 : Number((good * 100 / total).toFixed(2))
 
   const handleGoodClick = () => setGood(good + 1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
@@ -14,15 +18,18 @@ const App = () => {
 
   return (
     <div>
-      <Header text = 'give feedback'/>
-      <Button onClick = {handleGoodClick} text = 'good'/>
-      <Button onClick = {handleNeutralClick} text = 'neutral' />
-      <Button onClick = {handleBadClick} text = 'bad' />
-      
-      <Header text = 'statistics'/>
+      <Header text='give feedback' />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+
+      <Header text='statistics' />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
