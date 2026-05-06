be191e281a767a04b164de1299404ed6c008cfd0
WhiskeyCola <jackcygan@gmail.com>
2026-05-06 11:45:41 +0300

    part3 exercise 3.3 phonebook_backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -36,6 +36,17 @@ app.get('/info', (request, response) => {
         <p>${new Date()}</p>
         `)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if(!person) {
+        return response.status(404).json({error: '404 Not found'})
+    }
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
