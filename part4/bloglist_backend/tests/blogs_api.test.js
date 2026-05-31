268e978fb759bf6a07285b97de0c60b8d998eef5
WhiskeyCola <jackcygan@gmail.com>
2026-05-31 18:55:40 +0300

    part4 exercise 4.10 bloglist_backend add POST /api/blogs test

--- part4/bloglist_backend/tests/blogs_api.test.js
+++ part4/bloglist_backend/tests/blogs_api.test.js
@@ -5,6 +5,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const blogs = require('./test_blogs')
 const Blog = require('../models/blog')
+const { blogsInDb } = require('./test_helper')
 
 const api = supertest(app)
 
@@ -33,6 +34,25 @@ test('blogs have id field', async () => {
     assert(blog.id)
     assert.strictEqual(blog._id, undefined)
   })
+})
+
+test('a new blog can be added', async () => {
+  const newBlog = {
+    title: 'test',
+    author: 'Test Author',
+    url: 'Not exist',
+    likes: 0
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, blogs.length + 1)
+  const blogTitles = blogsAtEnd.map(blog => blog.title)
+  assert(blogTitles.includes(newBlog.title))
 })
 
 after(async () => {
