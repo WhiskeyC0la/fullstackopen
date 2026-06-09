576ae4bd0316481aa8acd901adf844641fef106f
WhiskeyCola <jackcygan@gmail.com>
2026-06-09 19:06:50 +0300

    part4 exercise 4.19 bloglist_backend require authentication for blog creation

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response, next) => {
   try{
@@ -12,14 +13,34 @@ blogsRouter.get('/', async (request, response, next) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
   try {
-    const user = await User.findById(request.body.user)
+    const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    if(!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     if(!user) {
       return response.status(400).json({ error: 'user id missing or not valid' })
     }
 
-    const blog = new Blog(request.body)
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      user: user._id
+    })
     const savedBlog = await blog.save()
 
     user.blogs = user.blogs.concat(savedBlog._id)
