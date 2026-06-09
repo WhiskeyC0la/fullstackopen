03be7a5aa5a65f4f4af5ef7ce1105f15ae3026d6
WhiskeyCola <jackcygan@gmail.com>
2026-06-09 21:31:12 +0300

    part4 exercise 4.21 bloglist_backend restrict blog deletion to owner

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -46,11 +46,21 @@ blogsRouter.post('/', async (request, response, next) => {
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    const deletedBlog = await Blog.findByIdAndDelete(request.params.id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    const blog = await Blog.findById(request.params.id)
 
-    if(!deletedBlog) {
+    if(!blog) {
       return response.status(404).end()
     }
+
+    if(!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    } else if(blog.user.toString() !== decodedToken.id) {
+      return response.status(403).json({ error: 'access denied' })
+    }
+
+    await Blog.findByIdAndDelete(request.params.id)
+
     response.status(204).end()
   } catch (error) {
     next(error)
