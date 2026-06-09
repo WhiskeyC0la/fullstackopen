cef521eb73c9b7f6aca2cd133a4c32a138d9b6bb
WhiskeyCola <jackcygan@gmail.com>
2026-06-09 20:11:14 +0300

    part4 exercise 4.20 bloglist_backend add token extractor middleware

--- part4/bloglist_backend/utils/middleware.js
+++ part4/bloglist_backend/utils/middleware.js
@@ -12,4 +12,12 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = errorHandler
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  request.token = authorization && authorization.startsWith('Bearer ')
+    ? authorization.replace('Bearer ', '')
+    : null
+  next()
+}
+
+module.exports = { errorHandler, tokenExtractor }
