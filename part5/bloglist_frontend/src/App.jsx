e9a70d381c99ff72f2aff946b6e4005b442706dd
WhiskeyCola <jackcygan@gmail.com>
2026-06-21 12:29:17 +0300

    part5 exercise 5.4 bloglist_frontend added notifications

--- part5/bloglist_frontend/src/App.jsx
+++ part5/bloglist_frontend/src/App.jsx
@@ -4,10 +4,13 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [messageType, setMessageType] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs => {
@@ -32,7 +35,11 @@ const App = () => {
       blogService.setToken(user.token)
       setUser(user)
     } catch (error) {
-      console.log(error.message)
+      setMessageType('error')
+      setMessage(error.response?.data?.error)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     }
   }
 
@@ -40,14 +47,37 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogappUser')
     setUser(null)
     blogService.setToken(null)
+    setMessageType('success')
+    setMessage(`${user.name} successfully logged out`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
   }
 
   const addBlog = async newBlog => {
     try {
-      const createdBlog = await blogService.create(newBlog)
-      setBlogs(blogs.concat(createdBlog))
+      const result = blogs.find(blog => blog.title === newBlog.title && blog.author === newBlog.author)
+      if(!result) {
+        const createdBlog = await blogService.create(newBlog)
+        setBlogs(blogs.concat(createdBlog))
+        setMessageType('success')
+        setMessage(`a new blog "${createdBlog.title}" by ${createdBlog.author} added`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+      } else {
+        setMessageType('error')
+        setMessage(`a blog "${newBlog.title}" by ${newBlog.author} already exists`)
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+      }
     } catch (error) {
-      console.log(error.message)
+      setMessageType('error')
+      setMessage(error.response?.data?.error || 'something went wrong')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     }
   }
 
@@ -55,6 +85,7 @@ const App = () => {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification message={message} type={messageType} />
         <LoginForm handleLogin={handleLogin}/>
       </div>
     )
@@ -63,10 +94,11 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-        <p>
-          {user.name} logged in
-          <button onClick={handleLogout}>logout</button>
-        </p>
+      <Notification message={message} type={messageType} />
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       <h2>create new</h2>
       <BlogForm addBlog={addBlog} />
       {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
