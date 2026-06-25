3ebe2b5c7641dc817293e0aeb6c8b4b39370c08e
WhiskeyCola <jackcygan@gmail.com>
2026-06-25 12:58:04 +0300

    part5 exercise 5.7 bloglist_frontend show/hide blog details

--- part5/bloglist_frontend/src/components/Blog.jsx
+++ part5/bloglist_frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const visibilityStyle = {
+    display: visible ? '' : 'none'
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+  <div style={blogStyle}>
+    <p>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>
+        {visible ? 'hide' : 'view'}
+      </button>
+    </p>
+    <div style={visibilityStyle}>
+      <p>{blog.url}</p>
+      <p>{blog.likes}</p>
+      <p>{blog.user.name}</p>
+    </div>
+  </div>
+)}
 
 export default Blog
