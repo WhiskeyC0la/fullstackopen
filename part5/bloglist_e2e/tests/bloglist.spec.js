75f6ecad37ec7d0125322142f1c178287f220ea0
WhiskeyCola <jackcygan@gmail.com>
2026-07-06 16:44:08 +0300

    part5 exercise 5.18 bloglist_e2e add login tests for valid and invalid credentials

--- part5/bloglist_e2e/tests/bloglist.spec.js
+++ part5/bloglist_e2e/tests/bloglist.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -19,5 +20,22 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', {name: 'login'})).toBeVisible()
+  })
+
+  describe('Login', () => {
+
+    test('succeeds with correct credentials',
+      async ({ page }) => {
+        await loginWith(page, 'jackflow', '345-987987')
+        await expect(page.getByText('Jack Flow logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials',
+      async ({page}) => {
+        await loginWith(page, 'jackflow', 'wrongpassword')
+        await expect(page.locator('.error')).toContainText('invalid username or password')
+        await expect(page.getByText('Jack Flow logged in')).not.toBeVisible()
+        await expect(page.getByRole('button', { name: 'login'})).toBeVisible()
+    })
   })
 })
