e322a98238935cb041936daa218ab9dc3e1129e2
WhiskeyCola <jackcygan@gmail.com>
2026-07-09 14:13:50 +0300

    part5 exercise 5.22 bloglist_e2e add test for remove button visibility by blog owner

--- part5/bloglist_e2e/tests/bloglist.spec.js
+++ part5/bloglist_e2e/tests/bloglist.spec.js
@@ -1,5 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith, createBlog } = require('./helper')
+const { loginWith, createBlog, expectLoginFormToBeVisible } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -15,11 +15,9 @@ describe('Blog app', () => {
     await page.goto('http://localhost:5173')
   })
 
-  test('Login form is shown', async ({ page }) => {
-    await expect(page.getByText('Log in to application')).toBeVisible()
-    await expect(page.getByLabel('username')).toBeVisible()
-    await expect(page.getByLabel('password')).toBeVisible()
-    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  test('Login form is shown',
+    async ({ page }) => {
+      await expectLoginFormToBeVisible(page)
   })
 
   describe('Login', () => {
@@ -43,32 +41,66 @@ describe('Blog app', () => {
         await createBlog(page, 'New blog with Playwright', 'Test Author', 'https://example.com')
       })
 
-      test('a new blog can be created', async ({ page }) => {
-        
-        await expect(page.getByText('New blog with Playwright Test Author')).toBeVisible()
-        //alternative solution with .locator()
-        //await expect(page.getByRole('button', { name: 'view' }).locator('..').filter({ hasText: 'New blog with Playwright' })).toBeVisible()
+      test('a new blog can be created',
+        async ({ page }) => {
+          await expect(page.getByText('New blog with Playwright Test Author')).toBeVisible()
+          //alternative solution with .locator()
+          //await expect(page.getByRole('button', { name: 'view' }).locator('..').filter({ hasText: 'New blog with Playwright' })).toBeVisible()
       })
 
-      test('can like the blog', async ({ page }) => {
-        const blog = page.getByText('New blog with Playwright Test Author').locator('..')
-        await blog.getByRole('button', { name: 'view' }).click()
-        await expect(blog.getByRole('button', { name: 'hide' })).toBeVisible()
-        await expect(blog.getByText('likes 0')).toBeVisible()
-        await blog.getByRole('button', { name: 'like' }).click()
-        await expect(blog.getByText('likes 1')).toBeVisible()
-        await expect(page.locator('.success'))
-          .toContainText('likes for New blog with Playwright by Test Author were successfully updated')
+      test('can like the blog',
+        async ({ page }) => {
+          const blog = page.getByText('New blog with Playwright Test Author').locator('..')
+          await blog.getByRole('button', { name: 'view' }).click()
+          await expect(blog.getByRole('button', { name: 'hide' })).toBeVisible()
+          await expect(blog.getByText('likes 0')).toBeVisible()
+          await blog.getByRole('button', { name: 'like' }).click()
+          await expect(blog.getByText('likes 1')).toBeVisible()
+          await expect(page.locator('.success'))
+            .toContainText('likes for "New blog with Playwright" by Test Author were successfully updated')
       })
 
-      test('user can remove a blog', async ({ page }) => {
-        const blog = page.getByText('New blog with Playwright Test Author').locator('..')
-        await blog.getByRole('button', { name: 'view' }).click()
-        page.on('dialog', dialog => dialog.accept())
-        await blog.getByRole('button', { name: 'remove' }).click()
-        await expect(page.locator('.success'))
-          .toContainText('Blog "New blog with Playwright" by Test Author was successfully removed')
-        await expect(blog).not.toBeVisible()
+      test('user can remove a blog',
+        async ({ page }) => {
+          const blog = page.getByText('New blog with Playwright Test Author').locator('..')
+          await blog.getByRole('button', { name: 'view' }).click()
+          page.on('dialog', dialog => dialog.accept())
+          await blog.getByRole('button', { name: 'remove' }).click()
+          await expect(page.locator('.success'))
+            .toContainText('Blog "New blog with Playwright" by Test Author was successfully removed')
+          await expect(blog).not.toBeVisible()
+      })
+
+      test('only the blog creator can see the remove button',
+        async ({ page, request }) => {
+          const newUser = {
+            name: 'John Doe',
+            username: 'secondOne',
+            password: 'confidential'
+          }
+          const newBlog = {
+            title: 'Another blog with Playwright',
+            author: 'Test User',
+            url: 'https://example.com'
+          }
+          await request.post('http://localhost:3003/api/users', {
+            data: newUser
+          })
+
+          await page.getByRole('button', { name: 'logout' }).click()
+          await expectLoginFormToBeVisible(page)
+          await loginWith(page, newUser.username, newUser.password)
+          await createBlog(page, newBlog.title, newBlog.author, newBlog.url)
+          
+          const blogByFirstUser = page.getByText('New blog with Playwright Test Author').locator('..')
+          
+          await blogByFirstUser.getByRole('button', { name: 'view' }).click()
+          await expect(blogByFirstUser.getByRole('button', { name: 'remove' })).not.toBeVisible()
+          
+          const blogBySecondUser = page.getByText(`${newBlog.title} ${newBlog.author}`).locator('..')
+          
+          await blogBySecondUser.getByRole('button', { name: 'view' }).click()
+          await expect(blogBySecondUser.getByRole('button', { name: 'remove' })).toBeVisible()
       })
     })
   })
--- part5/bloglist_e2e/tests/helper.js
+++ part5/bloglist_e2e/tests/helper.js
@@ -1,3 +1,5 @@
+const { expect } = require('@playwright/test')
+
 const loginWith = async (page, username, password) => {
   await page.getByLabel('username').fill(username)
   await page.getByLabel('password').fill(password)
@@ -11,4 +13,12 @@ const createBlog = async (page, title, author, url) => {
   await page.getByLabel('url').fill(url)
   await page.getByRole('button', { name: 'create' }).click()
 }
-module.exports = { loginWith, createBlog }
+
+const expectLoginFormToBeVisible = async (page) => {
+  await expect(page.getByText('Log in to application')).toBeVisible()
+  await expect(page.getByLabel('username')).toBeVisible()
+  await expect(page.getByLabel('password')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+}
+
+module.exports = { loginWith, createBlog, expectLoginFormToBeVisible }
