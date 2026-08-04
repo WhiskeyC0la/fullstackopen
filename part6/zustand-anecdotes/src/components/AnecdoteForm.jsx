fef25174a5d494fc5dccf14d7bdb7797d0fb4608
WhiskeyCola <jackcygan@gmail.com>
2026-08-04 12:53:21 +0300

    part6 exercise 6.4 anecdotes-zustand - refactor application into separate components

---
+++ part6/zustand-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+import { useAnecdoteActions } from '../store'
+
+const AnecdoteForm = () => {
+  const { add } = useAnecdoteActions()
+
+  const addAnecdote = event => {
+    event.preventDefault()
+    const content = event.currentTarget.anecdote.value.trim()
+    if(!content) return
+    add(content)
+    event.target.reset()
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name='anecdote'/>
+        </div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
