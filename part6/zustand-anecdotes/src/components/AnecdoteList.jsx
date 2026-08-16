706cea64b89f7bfdd1946c3613d7146ee58fd5cd
WhiskeyCola <jackcygan@gmail.com>
2026-08-16 12:44:05 +0300

    part6 exercise 6.11 anecdotes-zustand - implement anecdote deletion with Zustand and JSON Server

--- part6/zustand-anecdotes/src/components/AnecdoteList.jsx
+++ part6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,7 @@ import { useNotificationControl } from '../notificationStore'
 
 const AnecdoteList = () => {
   const anecdotes = useAnecdotes()
-  const { vote } = useAnecdoteActions()
+  const { vote, remove } = useAnecdoteActions()
   const { setNotification } = useNotificationControl()
   const sortedAnecdotes = anecdotes.toSorted((a, b) => b.votes - a.votes)
 
@@ -14,12 +14,19 @@ const AnecdoteList = () => {
           <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => {
-                  vote(anecdote.id)
+              <button onClick={async () => {
+                  await vote(anecdote.id)
                   setNotification(`You voted '${anecdote.content}'`)
                 }
                 }>vote
               </button>
+              {anecdote.votes === 0 && (
+              <button onClick={async () => {
+                await remove(anecdote.id)
+                setNotification(`Anecdote '${anecdote.content}' was successfully removed`)
+              }}>delete
+              </button>
+              )}
           </div>
         </div>
       ))}
--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -19,6 +19,12 @@ const useAnecdoteStore = create((set, get) => ({
       const newAnecdote = await anecdoteService.createNew(content)
       set(state => ({ anecdotes: state.anecdotes.concat(newAnecdote)}))
     },
+    remove: async id => {
+      await anecdoteService.remove(id)
+      set(state => ({
+        anecdotes: state.anecdotes.filter(a => a.id !== id)
+      }))
+    },
     setFilter: value => set({
       filter: value
     }),
