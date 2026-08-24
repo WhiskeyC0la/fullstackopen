4d66fc857a838b1eb2654f0ffe3e0cf7657bac3c
WhiskeyCola <jackcygan@gmail.com>
2026-08-24 14:07:02 +0300

    part6 exercise 6.17 query-anecdotes - add anecdote creation with TanStack Query mutation and cache update

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,22 @@
+import { createAnecdote } from "../requests"
+import { useQueryClient, useMutation } from "@tanstack/react-query"
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
+
   const onCreate = (event) => {
     event.preventDefault()
-    const content = event.target.anecdote.value
+    const content = event.target.anecdote.value.trim()
+    newAnecdoteMutation.mutate({ content, votes: 0 })
     event.target.reset()
-    console.log('new anecdote')
   }
 
   return (
