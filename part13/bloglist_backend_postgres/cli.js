fa1c50c6b3131f720824552580ca01969b25f128
WhiskeyCola <jackcygan@gmail.com>
2026-09-23 20:36:04 +0700

    Connect to PostgreSQL with Sequelize

---
+++ part13/bloglist_backend_postgres/cli.js
@@ -0,0 +1,26 @@
+require ('dotenv').config()
+const { Sequelize, QueryTypes } = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
+
+const main = async () => {
+  try {
+    const blogs = await sequelize.query('select * from blogs', { type: QueryTypes.SELECT })
+    blogs.forEach(blog =>
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    )
+    await sequelize.close()
+  } catch(error) {
+    console.error('Database error:', error.message)
+  } finally {
+    await sequelize.close()
+  }
+}
+
+main()
